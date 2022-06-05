a26f41e90f0ecf7155a2ef5c67ead33012c0db41
SaneenKP <saneenkp2000@gmail.com>
2022-06-05 19:07:32 +0530

    init implemented

---
+++ Constructor/init.kt
@@ -0,0 +1,22 @@
+class Box2(
+	val length: Int,
+	val width: Int,
+	val height: Int,
+){
+	
+	init{
+		println("init block called with length = ${length} , width = ${width} , height = ${height}")
+
+	}	
+
+	fun getValue(): Int{
+		return length*width*height
+
+	}
+}
+
+fun main(){
+	val box2 = Box2(5 , 10 , 15)
+	val area2 = box2.getValue()
+	println("area from parameterized constructor ${area2}")
+}
